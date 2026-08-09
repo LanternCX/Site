9b0f0abf08b2d5db227e8ffd38c3b6d0a5f5a956
Cao Xin <caoxin@xysu.tech>
2026-08-10 01:06:07 +0800

    fix: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = 'Cao Xin 的小站';
 export const SITE_BRAND = 'LANTERNCX';
 export const SITE_AUTHOR = 'Cao Xin';
-export const SITE_DESCRIPTION = '写点笔记，写点文章，也记录我正在做的事。';
+export const SITE_DESCRIPTION = '欢迎来到我的小站\\o/';
 export const SITE_KEYWORDS = ['typecho', 'php', 'blog'];
